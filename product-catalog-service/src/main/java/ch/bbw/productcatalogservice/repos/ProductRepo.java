2fda6d5f5285ead245f725745364257901699a78
jelenal1 <jabbruh10@gmail.com>
2024-05-21 19:35:00 +0200

    feat: Product Rest Service with h2 databse

---
+++ product-catalog-service/src/main/java/ch/bbw/productcatalogservice/repos/ProductRepo.java
@@ -0,0 +1,7 @@
+package ch.bbw.productcatalogservice.repos;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "products")
+public interface ProductRepo {
+}
