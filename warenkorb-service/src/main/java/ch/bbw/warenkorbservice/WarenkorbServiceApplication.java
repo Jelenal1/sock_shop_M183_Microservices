913066cf3f0da9391ac40d40ea89b5f97f55ecbd
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-05-22 16:04:30 +0200

    chore: setup warenkorb service

---
+++ warenkorb-service/src/main/java/ch/bbw/warenkorbservice/WarenkorbServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.warenkorbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenkorbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarenkorbServiceApplication.class, args);
+	}
+
+}
---
+++ warenkorb-service/src/test/java/ch/bbw/warenkorbservice/WarenkorbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.warenkorbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarenkorbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
